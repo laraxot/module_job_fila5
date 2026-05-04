17b1c4adac049477bf71d0bdaadced9e5aecc2e7
marco76tv <marco.sottana@gmail.com>
2026-05-04 16:21:19 +0200

    .

--- app/Models/Policies/ParameterPolicy.php
+++ app/Models/Policies/ParameterPolicy.php
@@ -4,10 +4,4 @@ declare(strict_types=1);
 
 namespace Modules\Job\Models\Policies;
 
-use Modules\Job\Models\Parameter;
-use Modules\Xot\Contracts\UserContract;
-
-class ParameterPolicy extends JobBasePolicy
-{
-   
-}
+class ParameterPolicy extends JobBasePolicy {}
