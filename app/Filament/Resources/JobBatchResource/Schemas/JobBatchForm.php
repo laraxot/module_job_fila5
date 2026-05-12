759bbf6d97c938f5b464a52361ffd294a103a3ea
marco76tv <marco.sottana@gmail.com>
2026-05-12 22:08:15 +0200

    .

--- app/Filament/Resources/JobBatchResource/Schemas/JobBatchForm.php
+++ app/Filament/Resources/JobBatchResource/Schemas/JobBatchForm.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Modules\Job\app\Filament\Resources\JobBatchResource\Schemas;
+namespace Modules\Job\Filament\Resources\JobBatchResource\Schemas;
 
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Components\Section;
--- app/Filament/Resources/JobsWaitingResource/Schemas/JobsWaitingForm.php
+++ app/Filament/Resources/JobsWaitingResource/Schemas/JobsWaitingForm.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Modules\Job\app\Filament\Resources\JobsWaitingResource\Schemas;
+namespace Modules\Job\Filament\Resources\JobsWaitingResource\Schemas;
 
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Components\Section;
