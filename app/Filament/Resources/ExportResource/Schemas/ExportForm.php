7cd650163dc13f94c38fe0c3cc830f7eaa0b73ad
marco76tv <marco.sottana@gmail.com>
2026-05-12 08:10:51 +0200

    .

--- app/Filament/Resources/ExportResource/Schemas/ExportForm.php
+++ app/Filament/Resources/ExportResource/Schemas/ExportForm.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Modules\base_quaeris_fila5\var\www\_bases\base_quaeris_fila5\laravel\Modules\Job\app\Filament\Resources\ExportResource\Schemas;
+namespace Modules\Job\app\Filament\Resources\ExportResource\Schemas;
 
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Components\Section;
--- app/Filament/Resources/JobManagerResource/Tables/JobManagersTable.php
+++ app/Filament/Resources/JobManagerResource/Tables/JobManagersTable.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Modules\base_quaeris_fila5\var\www\_bases\base_quaeris_fila5\laravel\Modules\Job\app\Filament\Resources\JobManagerResource\Tables;
+namespace Modules\Job\app\Filament\Resources\JobManagerResource\Tables;
 
 use Filament\Tables\Columns\Column;
 use Filament\Tables\Columns\TextColumn;
