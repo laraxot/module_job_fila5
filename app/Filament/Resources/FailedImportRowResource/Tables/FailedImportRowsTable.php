8018f6fbc75576b22b40d1a95f3a5e633d3b7627
marco76tv <marco.sottana@gmail.com>
2026-05-13 14:54:41 +0200

    .

---
+++ app/Filament/Resources/FailedImportRowResource/Tables/FailedImportRowsTable.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Modules\Job\Filament\Resources\FailedImportRowResource\Tables;
+
+use Filament\Tables\Columns\TextColumn;
+use Modules\Xot\Filament\Resources\Tables\XotBaseResourceTable;
+
+class FailedImportRowsTable extends XotBaseResourceTable
+{
+    public static function getTableColumns(): array
+    {
+        return [
+            'id' => TextColumn::make('id')->searchable()->sortable(),
+            'created_at' => TextColumn::make('created_at')->dateTime(),
+            'updated_at' => TextColumn::make('updated_at')->dateTime(),
+        ];
+    }
+}
