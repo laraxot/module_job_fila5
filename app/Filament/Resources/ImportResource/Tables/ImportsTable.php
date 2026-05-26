99014fecb90a62d3f608d50885f45877ebc2f6b5
Malebestia <malebestia@yahoo.it>
2026-05-26 11:52:55 +0200

    .

--- app/Filament/Resources/ImportResource/Tables/ImportsTable.php
+++ app/Filament/Resources/ImportResource/Tables/ImportsTable.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Modules\base_quaeris_fila5\var\www\_bases\base_quaeris_fila5\laravel\Modules\Job\app\Filament\Resources\ImportResource\Tables;
+namespace Modules\Job\app\Filament\Resources\ImportResource\Tables;
 
 use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
--- app/Filament/Resources/JobManagerResource/Tables/JobManagersTable.php
+++ app/Filament/Resources/JobManagerResource/Tables/JobManagersTable.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Modules\base_quaeris_fila5\var\www\_bases\base_quaeris_fila5\laravel\Modules\Job\app\Filament\Resources\JobManagerResource\Tables;
+namespace Modules\Job\app\Filament\Resources\JobManagerResource\Tables;
 
 use Filament\Actions\DeleteBulkAction;
 use Filament\Tables\Columns\Column;
