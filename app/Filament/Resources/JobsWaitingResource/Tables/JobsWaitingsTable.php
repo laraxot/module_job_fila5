43f027f0136ad85c19b2ddd263dec252e779c0ae
Malebestia <malebestia@yahoo.it>
2026-05-26 11:52:59 +0200

    .

--- app/Filament/Resources/JobsWaitingResource/Tables/JobsWaitingsTable.php
+++ app/Filament/Resources/JobsWaitingResource/Tables/JobsWaitingsTable.php
@@ -15,9 +15,6 @@ class JobsWaitingsTable extends XotBaseResourceTable
      */
     public function getTableColumns(): array
     {
-    /**
-     * @return array<int\|string, \Filament\Tables\Columns\Column>
-     */
         return [
             'id' => TextColumn::make('id')->sortable(),
             'queue' => TextColumn::make('queue')->searchable()->sortable(),
