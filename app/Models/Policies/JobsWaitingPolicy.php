53f248189dbb93f0084f321b012a5c6e42e79d26
marco76tv <marco.sottana@gmail.com>
2026-05-26 13:33:05 +0200

    .

--- app/Models/Policies/JobsWaitingPolicy.php
+++ app/Models/Policies/JobsWaitingPolicy.php
@@ -4,19 +4,4 @@ declare(strict_types=1);
 
 namespace Modules\Job\Models\Policies;
 
-<<<<<<< HEAD
 class JobsWaitingPolicy extends JobBasePolicy {}
-=======
-<<<<<<< Updated upstream
-use Modules\Job\Models\JobsWaiting;
-use Modules\Xot\Contracts\UserContract;
-
-class JobsWaitingPolicy extends JobBasePolicy
-{
-    
-=======
-class JobsWaitingPolicy extends JobBasePolicy
-{
->>>>>>> Stashed changes
-}
->>>>>>> 860dff1 (.)
