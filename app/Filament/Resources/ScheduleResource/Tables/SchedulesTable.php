2492f4333689f1ff23d67b21580d992c0db55247
Malebestia <malebestia@yahoo.it>
2026-05-26 11:52:59 +0200

    .

--- app/Filament/Resources/ScheduleResource/Tables/SchedulesTable.php
+++ app/Filament/Resources/ScheduleResource/Tables/SchedulesTable.php
@@ -11,9 +11,6 @@ class SchedulesTable extends XotBaseResourceTable
 {
     public function getTableColumns(): array
     {
-    /**
-     * @return array<int\|string, \Filament\Tables\Columns\Column>
-     */
         return [
             'id' => TextColumn::make('id')->searchable()->sortable(),
             'created_at' => TextColumn::make('created_at')->dateTime(),
