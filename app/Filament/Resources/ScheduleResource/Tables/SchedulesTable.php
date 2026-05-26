ba615f5585d2865b7b7d2403e61a8c86db078106
marco76tv <marco.sottana@gmail.com>
2026-05-26 15:54:34 +0200

    .

--- app/Filament/Resources/ScheduleResource/Tables/SchedulesTable.php
+++ app/Filament/Resources/ScheduleResource/Tables/SchedulesTable.php
@@ -9,7 +9,7 @@ use Modules\Xot\Filament\Resources\Tables\XotBaseResourceTable;
 
 class SchedulesTable extends XotBaseResourceTable
 {
-    public static function getTableColumns(): array
+    public function getTableColumns(): array
     {
         return [
             'id' => TextColumn::make('id')->searchable()->sortable(),
