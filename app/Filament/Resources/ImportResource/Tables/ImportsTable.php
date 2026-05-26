48dabe8ba025f32f3d779e9a5c0f1db5e3a6f109
Malebestia <malebestia@yahoo.it>
2026-05-26 11:52:55 +0200

    .

--- app/Filament/Resources/ImportResource/Tables/ImportsTable.php
+++ app/Filament/Resources/ImportResource/Tables/ImportsTable.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Modules\base_quaeris_fila5\var\www\_bases\base_quaeris_fila5\laravel\Modules\Job\app\Filament\Resources\ImportResource\Tables;
+namespace Modules\Job\app\Filament\Resources\ImportResource\Tables;
 
 use Filament\Tables\Columns\Column;
 use Filament\Tables\Columns\TextColumn;
