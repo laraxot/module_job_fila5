8844af4bf68cab9b230e0e689363a480e2a0e2d1
Malebestia <malebestia@yahoo.it>
2026-05-26 11:53:12 +0200

    .

--- app/Filament/Resources/ExportResource/Tables/ExportsTable.php
+++ app/Filament/Resources/ExportResource/Tables/ExportsTable.php
@@ -16,9 +16,17 @@ class ExportsTable extends XotBaseResourceTable
     public static function getTableColumns(): array
     {
         return [
-            'id' => TextColumn::make('id')->sortable(),
-            'name' => TextColumn::make('name')->searchable(),
+            'id' => TextColumn::make('id')->searchable()->sortable(),
+            'exporter' => TextColumn::make('exporter')->searchable()->sortable(),
+            'file_name' => TextColumn::make('file_name')->searchable()->sortable(),
+            'file_disk' => TextColumn::make('file_disk')->sortable(),
+            'processed_rows' => TextColumn::make('processed_rows')->sortable(),
+            'total_rows' => TextColumn::make('total_rows')->sortable(),
+            'successful_rows' => TextColumn::make('successful_rows')->sortable(),
+            'user_id' => TextColumn::make('user_id')->searchable()->sortable(),
+            'completed_at' => TextColumn::make('completed_at')->dateTime()->sortable(),
             'created_at' => TextColumn::make('created_at')->dateTime()->sortable(),
+            'updated_at' => TextColumn::make('updated_at')->dateTime()->sortable()->toggleable(isToggledHiddenByDefault: true),
         ];
     }
 }
