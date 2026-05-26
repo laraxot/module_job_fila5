cb66d5ff6c41e9b218a351d070637db1c8aaaf37
Malebestia <malebestia@yahoo.it>
2026-05-26 11:53:12 +0200

    .

--- app/Filament/Resources/FailedImportRowResource/Tables/FailedImportRowsTable.php
+++ app/Filament/Resources/FailedImportRowResource/Tables/FailedImportRowsTable.php
@@ -4,17 +4,23 @@ declare(strict_types=1);
 
 namespace Modules\Job\Filament\Resources\FailedImportRowResource\Tables;
 
+use Filament\Tables\Columns\Column;
 use Filament\Tables\Columns\TextColumn;
 use Modules\Xot\Filament\Resources\Tables\XotBaseResourceTable;
 
 class FailedImportRowsTable extends XotBaseResourceTable
 {
+    /**
+     * @return array<string, Column>
+     */
     public static function getTableColumns(): array
     {
         return [
             'id' => TextColumn::make('id')->searchable()->sortable(),
-            'created_at' => TextColumn::make('created_at')->dateTime(),
-            'updated_at' => TextColumn::make('updated_at')->dateTime(),
+            'import_id' => TextColumn::make('import_id')->sortable(),
+            'validation_error' => TextColumn::make('validation_error')->searchable(),
+            'created_at' => TextColumn::make('created_at')->dateTime()->sortable(),
+            'updated_at' => TextColumn::make('updated_at')->dateTime()->sortable()->toggleable(isToggledHiddenByDefault: true),
         ];
     }
 }
--- app/Filament/Resources/JobManagerResource/Tables/JobManagersTable.php
+++ app/Filament/Resources/JobManagerResource/Tables/JobManagersTable.php
@@ -16,9 +16,19 @@ class JobManagersTable extends XotBaseResourceTable
     public static function getTableColumns(): array
     {
         return [
-            'id' => TextColumn::make('id')->sortable(),
-            'name' => TextColumn::make('name')->searchable(),
+            'id' => TextColumn::make('id')->searchable()->sortable(),
+            'job_id' => TextColumn::make('job_id')->searchable()->sortable(),
+            'name' => TextColumn::make('name')->searchable()->sortable(),
+            'queue' => TextColumn::make('queue')->searchable()->sortable(),
+            'status' => TextColumn::make('status')->badge()->sortable(),
+            'failed' => TextColumn::make('failed')->badge()->sortable(),
+            'attempt' => TextColumn::make('attempt')->sortable(),
+            'progress' => TextColumn::make('progress')->sortable(),
+            'started_at' => TextColumn::make('started_at')->dateTime()->sortable(),
+            'finished_at' => TextColumn::make('finished_at')->dateTime()->sortable(),
+            'exception_message' => TextColumn::make('exception_message')->searchable(),
             'created_at' => TextColumn::make('created_at')->dateTime()->sortable(),
+            'updated_at' => TextColumn::make('updated_at')->dateTime()->sortable()->toggleable(isToggledHiddenByDefault: true),
         ];
     }
 }
