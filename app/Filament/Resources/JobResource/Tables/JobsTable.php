52ccbfbdb33ec52b1d01c902c2b6ad85164176c2
Malebestia <malebestia@yahoo.it>
2026-05-28 09:10:11 +0200

    .

--- app/Filament/Resources/JobResource/Tables/JobsTable.php
+++ app/Filament/Resources/JobResource/Tables/JobsTable.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Modules\Job\Filament\Resources\JobResource\Tables;
 
-<<<<<<< HEAD
 use Filament\Tables\Columns\Column;
 use Filament\Tables\Columns\TextColumn;
 use Modules\Xot\Filament\Resources\Tables\XotBaseResourceTable;
@@ -14,7 +13,7 @@ class JobsTable extends XotBaseResourceTable
     /**
      * @return array<string, Column>
      */
-    public static function getTableColumns(): array
+    public function getTableColumns(): array
     {
         return [
             'id' => TextColumn::make('id')->sortable(),
@@ -22,31 +21,6 @@ class JobsTable extends XotBaseResourceTable
             'attempts' => TextColumn::make('attempts')->sortable(),
             'available_at' => TextColumn::make('available_at')->dateTime()->sortable(),
             'created_at' => TextColumn::make('created_at')->dateTime()->sortable(),
-=======
-use Filament\Tables\Columns\TextColumn;
-use Filament\Tables\Table;
-use Modules\Xot\Filament\Resources\Tables\XotBaseResourceTable;
-
-/**
- * JobsTable Schema.
- */
-class JobsTable extends XotBaseResourceTable
-{
-    /**
-     * @return array<int|string, \Filament\Tables\Columns\Column>
-     */
-    public function getTableColumns(): array
-    {
-    /**
-     * @return array<int\|string, \Filament\Tables\Columns\Column>
-     */
-        return [
-            TextColumn::make('id')->sortable(),
-            TextColumn::make('queue')->searchable()->sortable(),
-            TextColumn::make('attempts')->sortable(),
-            TextColumn::make('available_at')->dateTime()->sortable(),
-            TextColumn::make('created_at')->dateTime()->sortable(),
->>>>>>> 860dff1 (.)
         ];
     }
 }
