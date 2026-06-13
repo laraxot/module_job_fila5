288e6b20676990965711e3a9b8b1ba21942bdcc7
marco76tv <marco.sottana@gmail.com>
2026-06-13 11:00:25 +0200

    .

--- tests/Unit/Events/PublicEventTest.php
+++ tests/Unit/Events/PublicEventTest.php
@@ -3,8 +3,6 @@
 declare(strict_types=1);
 
 namespace Modules\Job\Tests\Unit\Events;
-use ReflectionClass;
-
 use function Safe\class_uses;
 use Illuminate\Broadcasting\Channel;
 use Modules\Job\Events\PublicEvent;
@@ -16,7 +14,7 @@ uses(TestCase::class);
 
 describe('PublicEvent', function () {
     it('implements ShouldBroadcast', function () {
-        $interfaces = (new ReflectionClass(PublicEvent::class))->getInterfaceNames();
+        $interfaces = (new \ReflectionClass(PublicEvent::class))->getInterfaceNames();
 
         Assert::assertContains('Illuminate\Contracts\Broadcasting\ShouldBroadcast', $interfaces);
     });
@@ -36,7 +34,7 @@ describe('PublicEvent', function () {
     });
 
     it('has broadcastOn method', function () {
-        $reflection = new ReflectionClass(PublicEvent::class);
+        $reflection = new \ReflectionClass(PublicEvent::class);
         $method = $reflection->getMethod('broadcastOn');
 
         Assert::assertTrue($method->isPublic());
@@ -45,13 +43,13 @@ describe('PublicEvent', function () {
     });
 
     it('has correct namespace', function () {
-        $reflection = new ReflectionClass(PublicEvent::class);
+        $reflection = new \ReflectionClass(PublicEvent::class);
 
         Assert::assertSame('Modules\Job\Events', $reflection->getNamespaceName());
     });
 
     it('uses strict types', function () {
-        $reflection = new ReflectionClass(PublicEvent::class);
+        $reflection = new \ReflectionClass(PublicEvent::class);
         $filename = $reflection->getFileName();
 
         Assert::assertNotFalse($filename);
@@ -60,7 +58,7 @@ describe('PublicEvent', function () {
     });
 
     it('has required imports', function () {
-        $filename = (new ReflectionClass(PublicEvent::class))->getFileName();
+        $filename = (new \ReflectionClass(PublicEvent::class))->getFileName();
         Assert::assertNotFalse($filename);
         $content = file_get_contents($filename);
 
