a20be133124e8aaddc34fb17ce4943e6cd1f0bc6
marco76tv <marco.sottana@gmail.com>
2026-06-17 15:33:26 +0200

    .

--- app/Filament/Resources/ImportResource/Tables/ImportsTable.php
+++ app/Filament/Resources/ImportResource/Tables/ImportsTable.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Modules\Job\Filament\Resources\ImportResource\Tables;
 
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
 use Filament\Tables\Columns\Column;
 use Filament\Tables\Columns\TextColumn;
 use Modules\Xot\Filament\Resources\Tables\XotBaseResourceTable;
@@ -19,6 +21,20 @@ class ImportsTable extends XotBaseResourceTable
             'id' => TextColumn::make('id')->sortable(),
             'name' => TextColumn::make('name')->searchable(),
             'created_at' => TextColumn::make('created_at')->dateTime()->sortable(),
+        ];
+    }
+
+    public function getTableActions(): array
+    {
+        return [
+            'edit' => EditAction::make(),
+        ];
+    }
+
+    public function getTableBulkActions(): array
+    {
+        return [
+            'delete' => DeleteBulkAction::make(),
         ];
     }
 }
