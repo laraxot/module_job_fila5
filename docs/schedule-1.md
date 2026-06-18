0d307b8a0aadce75cc46fb930226c19bf64a0281
Malebestia <malebestia@yahoo.it>
2026-06-18 18:32:55 +0200

    .

--- docs/schedule-1.md
+++ docs/schedule-1.md
@@ -1,3 +1,12 @@
+<<<<<<< HEAD:docs/archive/schedule-1.md
+---
+module: theme
+topic: schedule-1
+canonical: ../../../Themes/docs/shared-components/.gitkeep
+---
+
+See canonical documentation: ../../../Themes/docs/shared-components/.gitkeep
+=======
 https://github.com/codestudiohq/laravel-totem
 
 https://github.com/stylers-llc/laravel-task-manager
@@ -14,3 +23,4 @@ https://filamentphp.com/plugins/mvenghaus-schedule-monitor
 
 
 
+>>>>>>> 2d797f1 (.):docs/schedule-1.md
--- lang/lang/it/failed_import_row.php
+++ lang/lang/it/failed_import_row.php
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 |<|?|p|h|p|
 |
 |d|e|c|l|a|r|e|(|s|t|r|i|c|t|_|t|y|p|e|s|=|1|)|;|
@@ -13,3 +14,41 @@
 | | | | |]|,|
 |]|;|
 |
+=======
+<?php
+
+declare(strict_types=1);
+
+return array (
+  'fields' => 
+  array (
+    'edit' => 
+    array (
+      'label' => 'edit',
+      'tooltip' => '',
+      'helper_text' => '',
+      'description' => '',
+    ),
+    'validation_error' => 
+    array (
+      'label' => 'validation_error',
+      'tooltip' => '',
+      'helper_text' => '',
+      'description' => '',
+    ),
+  ),
+  'navigation' => 
+  array (
+    'label' => 'Missing Navigation Label',
+    'plural_label' => 'Missing Navigation Plural Label',
+    'group' => 'Missing Group',
+    'icon' => 'heroicon-o-puzzle-piece',
+    'sort' => 100,
+  ),
+  'label' => 'Missing Label',
+  'plural_label' => 'Missing Plural label',
+  'actions' => 
+  array (
+  ),
+);
+>>>>>>> 2d797f1 (.)
