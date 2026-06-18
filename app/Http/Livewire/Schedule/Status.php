2ce293b6edf2c05d38067130540dc35d57ece8f7
Malebestia <malebestia@yahoo.it>
2026-06-18 18:33:29 +0200

    .

--- app/Http/Livewire/Schedule/Status.php
+++ app/Http/Livewire/Schedule/Status.php
@@ -72,10 +72,10 @@ class Status extends Component
 
     public function artisan(string $cmd): void
     {
-        $this->out .= '<hr/>';
+        // @var mixed out .= '<hr/>';
         Artisan::call($cmd);
-        $this->out .= Artisan::output();
-        $this->out .= '<hr/>';
+        // @var mixed out .= Artisan::output(;
+        // @var mixed out .= '<hr/>';
     }
 
     /**
