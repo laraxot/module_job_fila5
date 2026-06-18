4ca8cdb23b17ce0c8ba758df6bd7f8c9e1f1d440
Malebestia <malebestia@yahoo.it>
2026-06-18 18:33:46 +0200

    .

--- app/Filament/Resources/JobManagerResource/Widgets/JobStatsOverview.php
+++ app/Filament/Resources/JobManagerResource/Widgets/JobStatsOverview.php
@@ -1,5 +1,9 @@
 <?php
 
+/**
+ * ---.
+ */
+
 declare(strict_types=1);
 
 namespace Modules\Job\Filament\Resources\JobManagerResource\Widgets;
@@ -9,6 +13,7 @@ use Filament\Widgets\StatsOverviewWidget\Stat;
 use Illuminate\Support\Facades\DB;
 use Modules\Job\Models\JobManager;
 use Modules\Job\Traits\FormatSeconds;
+use Modules\Xot\Actions\Cast\SafeEloquentCastAction;
 
 class JobStatsOverview extends BaseWidget
 {
@@ -24,14 +29,31 @@ class JobStatsOverview extends BaseWidget
 
         $aggregatedInfo = JobManager::query()->select($aggregationColumns)->first();
 
-        $totalJobs = (int) ($aggregatedInfo->count ?? 0);
-        $totalTime = $this->formatSeconds((int) ($aggregatedInfo->total_time_elapsed ?? 0));
-        $averageTime = round((float) ($aggregatedInfo->average_time_elapsed ?? 0), 2).'s';
+        if ($aggregatedInfo) {
+            $averageTime = app(SafeEloquentCastAction::class)
+                ->getStringAttribute($aggregatedInfo, 'average_time_elapsed', '0')
+                ? ceil(
+                    (float) app(SafeEloquentCastAction::class)
+                        ->getStringAttribute($aggregatedInfo, 'average_time_elapsed', '0'),
+                ).'s'
+                : '0';
+
+            $totalTime = app(SafeEloquentCastAction::class)
+                ->getStringAttribute($aggregatedInfo, 'total_time_elapsed', '0')
+                ? $this->formatSeconds(
+                    (int) app(SafeEloquentCastAction::class)
+                        ->getStringAttribute($aggregatedInfo, 'total_time_elapsed', '0'),
+                )
+                : '0';
+        } else {
+            $averageTime = '0';
+            $totalTime = '0';
+        }
 
         return [
-            Stat::make('Totale Jobs', (string) $totalJobs),
-            Stat::make('Tempo Totale', $totalTime),
-            Stat::make('Media Esecuzione', $averageTime),
+            Stat::make((string) __('jobs::translations.total_jobs'), (int) ($aggregatedInfo->count ?? 0)),
+            Stat::make((string) __('jobs::translations.execution_time'), (string) $totalTime),
+            Stat::make((string) __('jobs::translations.average_time'), (string) $averageTime),
         ];
     }
 }
