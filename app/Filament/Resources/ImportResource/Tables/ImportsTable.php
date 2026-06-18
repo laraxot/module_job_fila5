bb4978d4a676d916037517b4d6a0bcd250c390e6
Malebestia <malebestia@yahoo.it>
2026-06-18 18:34:39 +0200

    .

--- app/Filament/Resources/ImportResource/Tables/ImportsTable.php
+++ app/Filament/Resources/ImportResource/Tables/ImportsTable.php
@@ -15,7 +15,7 @@ class ImportsTable extends XotBaseResourceTable
     /**
      * @return array<string, Column>
      */
-    public static function getTableColumns(): array
+    public function getTableColumns(): array
     {
         return [
             'id' => TextColumn::make('id')->searchable()->sortable(),
