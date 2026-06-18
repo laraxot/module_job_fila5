f89962ef73efa533a070a8c0a4af34adc4745fb2
Malebestia <malebestia@yahoo.it>
2026-06-18 18:34:44 +0200

    .

--- app/Models/Policies/FrequencyPolicy.php
+++ app/Models/Policies/FrequencyPolicy.php
@@ -4,19 +4,4 @@ declare(strict_types=1);
 
 namespace Modules\Job\Models\Policies;
 
-<<<<<<< HEAD
 class FrequencyPolicy extends JobBasePolicy {}
-=======
-<<<<<<< Updated upstream
-use Modules\Job\Models\Frequency;
-use Modules\Xot\Contracts\UserContract;
-
-class FrequencyPolicy extends JobBasePolicy
-{
-    
-=======
-class FrequencyPolicy extends JobBasePolicy
-{
->>>>>>> Stashed changes
-}
->>>>>>> 860dff1 (.)
--- docs/testing-guidelines.md
+++ docs/testing-guidelines.md
@@ -1,1 +1,7 @@
+---
+module: theme
+topic: testing-guidelines
+canonical: ../../../Themes/docs/shared-components/.gitkeep
+---
 
+See canonical documentation: ../../../Themes/docs/shared-components/.gitkeep
