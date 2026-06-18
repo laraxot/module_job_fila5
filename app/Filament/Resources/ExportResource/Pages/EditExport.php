dde446ad5a7b0045487270b8b6d7a1db5c4ffd31
Malebestia <malebestia@yahoo.it>
2026-06-18 18:34:16 +0200

    .

--- app/Filament/Resources/ExportResource/Pages/EditExport.php
+++ app/Filament/Resources/ExportResource/Pages/EditExport.php
@@ -9,5 +9,5 @@ use Modules\Xot\Filament\Resources\Pages\XotBaseEditRecord;
 
 class EditExport extends XotBaseEditRecord
 {
-    public static string $resource = ExportResource::class;
+    protected static string $resource = ExportResource::class;
 }
--- app/Filament/Resources/FailedImportRowResource/Pages/EditFailedImportRow.php
+++ app/Filament/Resources/FailedImportRowResource/Pages/EditFailedImportRow.php
@@ -9,5 +9,5 @@ use Modules\Xot\Filament\Resources\Pages\XotBaseEditRecord;
 
 class EditFailedImportRow extends XotBaseEditRecord
 {
-    public static string $resource = FailedImportRowResource::class;
+    protected static string $resource = FailedImportRowResource::class;
 }
