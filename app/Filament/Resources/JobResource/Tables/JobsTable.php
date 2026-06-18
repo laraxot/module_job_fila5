7a09a4b667f743ee7c711a4b25f71bd9606a4d08
Malebestia <malebestia@yahoo.it>
2026-06-18 18:30:39 +0200

    .

--- app/Filament/Resources/JobResource/Tables/JobsTable.php
+++ app/Filament/Resources/JobResource/Tables/JobsTable.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Modules\Job\Filament\Resources\JobResource\Tables;
 
+<<<<<<< HEAD
 use Filament\Actions\DeleteAction;
 use Filament\Actions\ViewAction;
 use Filament\Tables\Columns\Column;
@@ -45,6 +46,28 @@ class JobsTable extends XotBaseResourceTable
         return [
             'view' => ViewAction::make(),
             'delete' => DeleteAction::make(),
+=======
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Modules\Xot\Filament\Resources\Tables\XotBaseResourceTable;
+
+/**
+ * JobsTable Schema.
+ */
+class JobsTable extends XotBaseResourceTable
+{
+    /**
+     * @return array<int|string, \Filament\Tables\Columns\Column>
+     */
+    public static function getTableColumns(): array
+    {
+        return [
+            TextColumn::make('id')->sortable(),
+            TextColumn::make('queue')->searchable()->sortable(),
+            TextColumn::make('attempts')->sortable(),
+            TextColumn::make('available_at')->dateTime()->sortable(),
+            TextColumn::make('created_at')->dateTime()->sortable(),
+>>>>>>> 860dff1 (.)
         ];
     }
 }
--- app/Models/Policies/ExportPolicy.php
+++ app/Models/Policies/ExportPolicy.php
@@ -4,4 +4,19 @@ declare(strict_types=1);
 
 namespace Modules\Job\Models\Policies;
 
+<<<<<<< HEAD
 class ExportPolicy extends JobBasePolicy {}
+=======
+<<<<<<< Updated upstream
+use Modules\Job\Models\Export;
+use Modules\Xot\Contracts\UserContract;
+
+class ExportPolicy extends JobBasePolicy
+{
+    
+=======
+class ExportPolicy extends JobBasePolicy
+{
+>>>>>>> Stashed changes
+}
+>>>>>>> 860dff1 (.)
