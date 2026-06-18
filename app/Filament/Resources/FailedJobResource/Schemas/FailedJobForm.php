6003aa7ba3fc27c453f35115e84cc59ebcb61f71
Malebestia <malebestia@yahoo.it>
2026-06-18 18:30:55 +0200

    .

--- app/Filament/Resources/FailedJobResource/Schemas/FailedJobForm.php
+++ app/Filament/Resources/FailedJobResource/Schemas/FailedJobForm.php
@@ -6,7 +6,7 @@ namespace Modules\Job\Filament\Resources\FailedJobResource\Schemas;
 
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Component;
+use Filament\Schemas\Components\Component;
 use Modules\Xot\Filament\Resources\Schemas\XotBaseResourceForm;
 
 /**
