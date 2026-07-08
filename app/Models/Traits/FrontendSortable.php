2f3c40189929e32408ff857990fe0a5e94e227ba
marco76tv <marco.sottana@gmail.com>
2026-07-08 08:45:49 +0200

    .

--- app/Models/Traits/FrontendSortable.php
+++ app/Models/Traits/FrontendSortable.php
@@ -11,7 +11,7 @@ trait FrontendSortable
     /**
      * @param  Builder<static>  $query
      * @param  array<string>  $sortableColumns
-     * @param  array<string, string>  $defaultSort
+     * @param  array<string, 'asc'|'desc'>  $defaultSort
      * @return Builder<static>
      */
     public function scopeSortableBy(
