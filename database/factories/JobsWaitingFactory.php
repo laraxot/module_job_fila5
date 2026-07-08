f1776737a64069da8d3a23db292a9a455c71ee12
marco76tv <marco.sottana@gmail.com>
2026-07-08 23:20:49 +0200

    .

--- database/factories/JobsWaitingFactory.php
+++ database/factories/JobsWaitingFactory.php
@@ -21,10 +21,9 @@ class JobsWaitingFactory extends Factory
 
     /**
      * Define the model's default state.
-<<<<<<< HEAD
-=======
+     */
+    /**
      * @return array<string, mixed>
->>>>>>> laraxot/dev
      */
     public function definition(): array
     {
--- database/factories/TaskCommentFactory.php
+++ database/factories/TaskCommentFactory.php
@@ -21,10 +21,9 @@ class TaskCommentFactory extends Factory
 
     /**
      * Define the model's default state.
-<<<<<<< HEAD
-=======
+     */
+    /**
      * @return array<string, mixed>
->>>>>>> laraxot/dev
      */
     public function definition(): array
     {
