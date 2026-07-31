4f26c504edca1a29382b645e026d26b2cbfd1156
marco76tv <marco.sottana@gmail.com>
2026-07-31 13:11:33 +0200

    .

--- app/Console/Commands/ScheduleClearCacheCommand.php
+++ app/Console/Commands/ScheduleClearCacheCommand.php
@@ -28,7 +28,13 @@ class ScheduleClearCacheCommand extends Command
      */
     public function handle(): int
     {
+<<<<<<< HEAD
         app(ClearScheduleCacheAction::class)->execute();
+=======
+        /** @var ClearScheduleCacheAction $action */
+        $action = app(ClearScheduleCacheAction::class);
+        $action->execute();
+>>>>>>> 6cee1df (.)
         $this->info('Scheduling cache cleared.');
 
         return 0;
--- config/config.php
+++ config/config.php
@@ -4,5 +4,9 @@ declare(strict_types=1);
 
 return [
     'name' => 'Job',
+<<<<<<< HEAD
     'icon' => 'heroicon-o-briefcase',
+=======
+    'icon' => 'job-icon',
+>>>>>>> 6cee1df (.)
 ];
