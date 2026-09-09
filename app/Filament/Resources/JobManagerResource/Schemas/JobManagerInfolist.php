7ca51b8d05d3663170a67d400d96cb9250b8838d
Marco Xot <marco76tv@hotmail.com>
2026-09-09 16:27:18 +0200

    .

--- app/Filament/Resources/JobManagerResource/Schemas/JobManagerInfolist.php
+++ app/Filament/Resources/JobManagerResource/Schemas/JobManagerInfolist.php
@@ -13,7 +13,7 @@ class JobManagerInfolist extends XotBaseResourceInfolist
     /**
      * @return array<string, Component>
      */
-    public static function getInfolistSchema(): array
+    public function getInfolistSchema(): array
     {
         return [
             'job_id' => TextEntry::make('job_id'),
