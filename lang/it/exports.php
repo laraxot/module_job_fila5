e224501628db3f2fde5271c94fb5d8237a2cebee
Marco Xot <marco76tv@hotmail.com>
2026-09-11 19:08:11 +0200

    .

--- lang/it/exports.php
+++ lang/it/exports.php
@@ -13,4 +13,9 @@ return [
         'successful_rows' => ['label' => 'successful_rows'],
         'completed_at' => ['label' => 'completed_at'],
     ],
+    'actions' => [
+        'view' => ['label' => 'view', 'icon' => 'view', 'tooltip' => 'view'],
+        'edit' => ['label' => 'edit', 'icon' => 'edit', 'tooltip' => 'edit'],
+        'delete' => ['label' => 'delete', 'icon' => 'delete', 'tooltip' => 'delete'],
+    ],
 ];
