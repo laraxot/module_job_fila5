1b16ff93a4428e47e70b02c0aea66ecbd2a9d96a
Marco Xot <marco76tv@hotmail.com>
2026-09-11 19:08:11 +0200

    .

--- lang/it/job_managers.php
+++ lang/it/job_managers.php
@@ -14,4 +14,9 @@ return [
         'started_at' => ['label' => 'started_at'],
         'finished_at' => ['label' => 'finished_at'],
     ],
+    'actions' => [
+        'view' => ['label' => 'view', 'icon' => 'view', 'tooltip' => 'view'],
+        'edit' => ['label' => 'edit', 'icon' => 'edit', 'tooltip' => 'edit'],
+        'delete' => ['label' => 'delete', 'icon' => 'delete', 'tooltip' => 'delete'],
+    ],
 ];
