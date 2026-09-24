cdfcf8ec3a870e310c93c2298ec2af6d6f87a149
Marco Xot <marco.sottana@gmail.com>
2026-09-24 07:05:33 +0000

    .

---
+++ lang/it/schedule_infolist.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'fields' => [
+        'command' => ['label' => 'command'],
+        'command_custom' => ['label' => 'command_custom'],
+        'params' => ['label' => 'params'],
+        'options' => ['label' => 'options'],
+        'options_with_value' => ['label' => 'options_with_value'],
+        'expression' => ['label' => 'expression'],
+        'even_in_maintenance_mode' => ['label' => 'even_in_maintenance_mode'],
+        'without_overlapping' => ['label' => 'without_overlapping'],
+        'on_one_server' => ['label' => 'on_one_server'],
+        'webhook_before' => ['label' => 'webhook_before'],
+        'webhook_after' => ['label' => 'webhook_after'],
+        'email_output' => ['label' => 'email_output'],
+        'sendmail_error' => ['label' => 'sendmail_error'],
+        'sendmail_success' => ['label' => 'sendmail_success'],
+        'log_success' => ['label' => 'log_success'],
+        'log_error' => ['label' => 'log_error'],
+        'status' => ['label' => 'status'],
+        'run_in_background' => ['label' => 'run_in_background'],
+        'log_filename' => ['label' => 'log_filename'],
+        'environments' => ['label' => 'environments'],
+    ],
+];
